89ba6739ab42c4efacbcf85002e60fea9816f6db
Daniel Gempesaw <gempesaw@gmail.com>
2021-04-13 22:04:06 +0000

    Memorize the environment variables window per pipeline

--- extension/src/inject/buildkite-new-build-branch-helper.js
+++ extension/src/inject/buildkite-new-build-branch-helper.js
@@ -1,5 +1,7 @@
 /* global chrome */
 
+const BK_ENV_VAR_PREFIX = 'bkEnvVarPrefix-';
+
 const doTheThing = () => {
   const parentSelectors = [
     'div[data-testid=PipelineHeader]',
@@ -16,7 +18,7 @@ const doTheThing = () => {
     subtree: true
   };
 
-  const observer = new MutationObserver(helpMeBranchHelper);
+  const observer = new MutationObserver(helpMeNewBuildHelper);
   observer.observe(parentElem, observeConfig);
 
   return observer;
@@ -26,16 +28,41 @@ const getAnElementProbably = (selectors) => selectors
   .map((selector) => document.querySelectorAll(selector)[0])
   .find(identity);
 
-const helpMeBranchHelper = (mutationList, observer) => {
-  if (mutationList.some(probablyClickedNewBuild)) {
-    const branchInputSelector = 'input[name="build[branch]"]';
-    const branchInput = getAnElementProbably([branchInputSelector]);
-    if (!branchInput) {
-      return;
-    }
 
-    const imHelping = findABranch();
-    branchInput.value = imHelping;
+const fillInBuildFromPage = (mutationList, observer) => {
+  const branchInputSelector = 'input[name="build[branch]"]';
+  const branchInput = getAnElementProbably([branchInputSelector]);
+  if (!branchInput) {
+    return;
+  }
+
+  const imHelping = findABranch();
+  branchInput.value = imHelping;
+};
+
+const makeEnvironmentVariablesSticky = (mutationList, observer) => {
+  const detailsInput = document.querySelectorAll('.details-reset')[0];
+  const envVarTextarea = document.querySelectorAll('textarea[name="build[env]"]')[0];
+  if (!detailsInput || !envVarTextarea) {
+    return;
+  }
+
+  const localStorageKeyWithSlug = `${ BK_ENV_VAR_PREFIX }${window.location.pathname.split('/').reverse()[0]}`;
+  const imHelping = window.localStorage[localStorageKeyWithSlug] || '';
+  if (imHelping) {
+    detailsInput.setAttribute('open', true);
+    envVarTextarea.value = imHelping;
+  }
+
+  envVarTextarea.addEventListener("change", ({ target: { value } }) => {
+    window.localStorage[localStorageKeyWithSlug] = value;
+  });
+};
+
+const helpMeNewBuildHelper = (mutationList, observer) => {
+  if (mutationList.some(probablyClickedNewBuild)) {
+    fillInBuildFromPage(mutationList, observer);
+    makeEnvironmentVariablesSticky(mutationList, observer);
   }
 };
 
